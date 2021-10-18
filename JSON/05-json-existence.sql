a995163f3f7b128399c0362cad85463065396cfe
Elijah Andrade <eandrade.a1@gmail.com>
2021-10-17 17:05:13 -0700

    JSON

--- JSON/05-json-existence.sql
+++ JSON/05-json-existence.sql
@@ -1,2 +1,3 @@
 SELECT CAST('{"name": "Bob", "favoriteFoods": ["Banana", "Candy"]}' AS JSONB) -> 'favoriteFoods' ? 'Banana';
 SELECT (CAST('{"name": "Bob", "favoriteFoods": ["Banana", "Candy"]}' AS JSONB) -> 'favoriteFoods') ? 'Banana';
+SELECT (CAST('{"name": "Bob", "favoriteFoods": ["Banana", "Candy"]}' AS JSONB) -> 'favoriteFoods') ? 'Apple';
