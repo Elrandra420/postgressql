e302bd78063210f2e1454a0d02a00344c0ec79ba
Elijah Andrade <eandrade.a1@gmail.com>
2021-10-24 19:11:04 -0700

    Filtering

--- filtering-with-/02-and-or.sql
+++ filtering-with-/02-and-or.sql
@@ -1,4 +1,4 @@
 -- SELECT <expression> AS <alias> FROM <table> WHERE <criteria> AND/OR <criteria>;
 
-SELECT pfirstname, plastname, pstate, pcity FROM  people WHERE pstate = 'NY' AND pcity = 'Cincinnati';
+-- SELECT pfirstname, plastname, pstate, pcity FROM  people WHERE pstate = 'NY' AND pcity = 'Cincinnati';
 SELECT pfirstname, plastname, pstate, pcity FROM  people WHERE pstate = 'NY' OR pcity = 'Cincinnati';
