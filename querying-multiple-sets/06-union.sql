47b339599b68ea3030ae710bda9abe0b7faf7040
Elijah Andrade <eandrade.a1@gmail.com>
2021-10-26 09:41:40 -0700

    JOIN

--- querying-multiple-sets/06-union.sql
+++ querying-multiple-sets/06-union.sql
@@ -1,2 +1,4 @@
 -- UNION
 SELECT cname FROM customers UNION SELECT mname FROM movies;
+SELECT cname FROM customers UNION SELECT mname FROM movies ORDER BY cname;
+SELECT cname AS "RESULTS" FROM customers UNION SELECT mname FROM movies ORDER BY "RESULTS";
