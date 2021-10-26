3fbd4662a40e1aa9a21bad7523457ec16e9c9b92
Elijah Andrade <eandrade.a1@gmail.com>
2021-10-26 10:47:33 -0700

    JOIN

--- querying-multiple-sets/09-natural.sql
+++ querying-multiple-sets/09-natural.sql
@@ -5,8 +5,10 @@ SELECT customers.cname, movies.mname FROM customers NATURAL JOIN rentals NATURAL
 -- SELECT customers.cname, movies.mname FROM customers
 --  JOIN rentals USING(cid) JOIN movies USING(mid);
 
+-- This might be the most common way to join/most clear way
 -- SELECT customers.cname, movies.mname FROM customers
 --  JOIN rentals ON (customers.cid=rentals.cid) JOIN movies ON (rentals.mid=movies.mid);
 
+-- Does not work with left or right join
 -- SELECT customers.cname, movies.mname FROM customers, rentals, movies
 --  WHERE customers.cid=rentals.cid AND rentals.mid=movies.mid;
